7ec9d81d7e86490684c2f4f80d545eb005f67d48
Stefano Gaggero <stefanolucagaggero@gmail.com>
2020-06-10 13:23:12 -0300

    Agregar archivos de proyecto.

---
+++ operaciones/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1, sum2, total;
+            Console.WriteLine("Ingrese un número:");
+            sum1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número:");
+            sum2 = int.Parse(Console.ReadLine());
+            total = sum1 + sum2;
+            Console.WriteLine("El resultado es: " + total);
+        }
+    }
+}
